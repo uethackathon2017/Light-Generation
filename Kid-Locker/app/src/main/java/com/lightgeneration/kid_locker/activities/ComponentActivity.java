e56a9e6ad2ccdc72a6d9ba8835e55ad828ef8534
Pham Van Long <phamvanlong021096@gmail.com>
2017-03-10 19:33:52 +0700

    Long's commit

---
+++ Kid-Locker/app/src/main/java/com/lightgeneration/kid_locker/activities/ComponentActivity.java
@@ -0,0 +1,107 @@
+package com.lightgeneration.kid_locker.activities;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.lightgeneration.kid_locker.R;
+import com.lightgeneration.kid_locker.fragments.InfoFragment;
+
+/**
+ * Created by PhamVanLong on 3/10/2017.
+ */
+
+public class ComponentActivity extends AppCompatActivity implements View.OnClickListener, NavigationView.OnNavigationItemSelectedListener {
+    private ImageView btnMenu;
+    private DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_component);
+        initComponents();
+    }
+
+    private void initComponents() {
+        btnMenu = (ImageView) findViewById(R.id.btn_menu_toolbar);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        btnMenu.setOnClickListener(this);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_menu_toolbar: {
+                drawerLayout.openDrawer(GravityCompat.START);
+                break;
+            }
+
+            default: {
+                break;
+            }
+        }
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        if (fragment.isVisible()) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.rl_content, fragment)
+                    .commit();
+            return;
+        }
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.rl_content, fragment)
+                .addToBackStack(null)
+                .commit();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_lock: {
+                break;
+            }
+
+            case R.id.menu_info: {
+                openInfoFragment();
+                break;
+            }
+
+            case R.id.menu_graph: {
+                break;
+            }
+
+            case R.id.menu_follow: {
+                break;
+            }
+
+            case R.id.menu_setting: {
+                break;
+            }
+
+            default: {
+                break;
+            }
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private void openInfoFragment() {
+        InfoFragment infoFragment = new InfoFragment();
+        replaceFragment(infoFragment);
+    }
+}
---
+++ Kid-Locker/app/src/main/java/com/lightgeneration/kid_locker/fragments/InfoFragment.java
@@ -0,0 +1,53 @@
+package com.lightgeneration.kid_locker.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.lightgeneration.kid_locker.R;
+
+/**
+ * Created by PhamVanLong on 3/10/2017.
+ */
+
+public class InfoFragment extends Fragment implements View.OnClickListener{
+    private Button btnSignUp;
+    private Button btnUpdate;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.info_fragment, container, false);
+        initComponents(view);
+        return view;
+    }
+
+    private void initComponents(View view) {
+        btnSignUp = (Button) view.findViewById(R.id.btn_sign_up);
+        btnUpdate = (Button) view.findViewById(R.id.btn_update);
+
+        btnSignUp.setOnClickListener(this);
+        btnUpdate.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_sign_up : {
+                break;
+            }
+
+            case R.id.btn_update: {
+                break;
+            }
+
+            default: {
+                break;
+            }
+        }
+    }
+}
